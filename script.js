eee1048a2d8d7ad50edad8699d2b9c44f4a01435
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-11-07 13:26:12 +0100

    Added eventlistener for letter press

--- script.js
+++ script.js
@@ -37,4 +37,46 @@ function displayWord() {
   }
 }
 
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("Update wrong");
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener("keydown", (e) => {
+  //console.log(e.key);
+  //console.log(e.code);
+
+  if (/^[a-zA-Z]$/.test(e.key)) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
 displayWord();
